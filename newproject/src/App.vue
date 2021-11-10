e486cc028f46037b7677236bc4016a7d8e983c9b
PaoloPascucci <pascuccip9@gmail.com>
2021-11-10 14:46:21 +0100

    Add newproject

---
+++ newproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  <h1>
+    <newTitle/>
+  </h1>
+  </div>
+</template>
+
+<script>
+import newTitle from '@/components/newTitle.vue';
+
+export default {
+  
+  components: {
+    newTitle
+  }
+}
+</script>
+
+<style>
+h1{
+  text-align: center;
+}
+</style>
